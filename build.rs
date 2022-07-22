85fd04a30f51bc2ecfa23f0f99124a8087baf3cf
Nugine <nugine@foxmail.com>
2022-07-22 21:30:21 +0800

    fmt: max_width

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 fn main() {
-    cc::Build::new()
-        .file("cpp/simdutfrs.cpp")
-        .compile("simdutfrs");
+    cc::Build::new().file("cpp/simdutfrs.cpp").compile("simdutfrs");
 
     // FIXME: https://github.com/simdutf/simdutf/issues/158
     println!("cargo:rustc-link-lib=dylib=stdc++");
