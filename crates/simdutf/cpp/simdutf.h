a5ce23cfd6796e799c186292d206d33227b5de57
Nugine <nugine@foxmail.com>
2023-04-10 12:17:27 +0800

    update vendor
    https://github.com/simdutf/simdutf/tree/09a122ffaeedc3b25186085d127ec9fb6ef57cb6

--- crates/simdutf/cpp/simdutf.h
+++ crates/simdutf/cpp/simdutf.h
@@ -1,4 +1,4 @@
-/* auto-generated on 2023-04-05 11:59:59 -0400. Do not edit! */
+/* auto-generated on 2023-04-09 17:10:44 -0400. Do not edit! */
 
 /* begin file include/simdutf.h */
 #ifndef SIMDUTF_H
@@ -574,7 +574,7 @@ SIMDUTF_DISABLE_UNDESIRED_WARNINGS
 #define SIMDUTF_SIMDUTF_VERSION_H
 
 /** The version of simdutf being used (major.minor.revision) */
-#define SIMDUTF_VERSION "3.2.4"
+#define SIMDUTF_VERSION "3.2.8"
 
 namespace simdutf {
 enum {
@@ -589,7 +589,7 @@ enum {
   /**
    * The revision (major.minor.REVISION) of simdutf being used.
    */
-  SIMDUTF_VERSION_REVISION = 4
+  SIMDUTF_VERSION_REVISION = 8
 };
 } // namespace simdutf
 
