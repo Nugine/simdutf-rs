080746fe569b7e9e5426c560c15d3c1eb1eb850b
Nugine <nugine@foxmail.com>
2023-05-13 13:53:11 +0800

    ci: notify

---
+++ scripts/check-release.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import requests
+
+repo = "simdutf/simdutf"
+
+url = f"https://github.com/{repo}/releases/latest"
+r = requests.get(url)
+version = r.url.split("/")[-1]
+
+current = version
+last = "v3.2.8"
+print(f"current: {current}")
+print(f"last:    {last}")
+assert current == last
