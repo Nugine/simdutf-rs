fff9a66e020557fb9a0db5c3db401bd291626a65
Nugine <nugine@foxmail.com>
2023-06-01 00:49:00 +0800

    docs: update
    resolves #1
    related with #6

--- crates/simdutf/src/lib.rs
+++ crates/simdutf/src/lib.rs
@@ -1,6 +1,27 @@
 //! Unicode validation and transcoding at billions of characters per second.
 //!
-//! This crate is the Rust binding of [simdutf](https://github.com/simdutf/simdutf).
+//! This crate is the Rust binding of [simdutf].
+//!
+//! ## Compilation
+//!
+//! This crate works out of the box as long as
+//! you have a C++11-compatible toolchain installed correctly.
+//!
+//! [simdutf] links C++ standard library, which adds a dynamic linking dependency.
+//!
+//! For more details, see [simdutf] documentation and [cc] documentation.
+//!
+//! Here is an example for local benchmark:
+//!
+//! ```bash
+//! export RUSTFLAGS='-C target-cpu=native'
+//! export CXXFLAGS='-march=native'
+//! cargo build --release
+//! ```
+//!
+//! [simdutf]: https://github.com/simdutf/simdutf
+//! [cc]: https://github.com/rust-lang/cc-rs
+//!
 
 #![deny(
     missing_docs,
